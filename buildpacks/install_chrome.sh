822fcd839942054dd66557ae6ab1a3e98c546dfe
dsubira <dsubira7@gmail.com>
2025-01-28 11:46:04 +0100

    Delete buildpacks/install_chrome.sh

--- buildpacks/install_chrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Configurant repositori de Google Chrome..."
-
-# Afegir la clau GPG de Google Chrome
-curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
